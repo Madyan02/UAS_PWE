3ff2ebffcbcc345927afe534bd415cb6abd312fe
Madyan02 <afafimadyan94@gmail.com>
2025-01-01 12:07:42 +0700

    pertama

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>Fresh Vegetile © {{ date('Y') }} || Developed by <strong><a target="_blank" href="">Kelompok 2</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
